1d6335e0310072643c93657c00938046c69ef47a
AlphaSgh <upupsgh@163.com>
2023-10-28 17:09:58 +0800

    c 语言实现面向对象思想

---
+++ c_study/datastruce/list/double_list/lib3/llist.h
@@ -0,0 +1,32 @@
+#ifndef __LLIST_H__
+#define __LLIST_H__
+
+#define LLIST_FORWARD    1
+#define LLIST_BACKWARD   2
+
+typedef void llist_op(const void *);
+typedef int llist_cmp(const void *, const void *);
+
+typedef struct llist_node_st
+{
+    struct llist_node_st *prev;
+    struct llist_node_st *next;
+    char data[1];
+} llist_node_st;
+
+typedef struct llist_head
+{
+    int size;
+    struct llist_node_st head;
+    int (*insert) (struct llist_head *, const void *, int);
+    void *(*find) (struct llist_head *, const void *, llist_cmp *);
+    int (*delete) (struct llist_head *, const void *, llist_cmp *);
+    int (*fetch) (struct llist_head *, const void *, llist_cmp *, void *);
+    void (*travel) (struct llist_head *, llist_op *);
+} LLIST;
+
+LLIST *llist_create(int initsize);
+
+void llist_destory(LLIST *llist);
+
+#endif
