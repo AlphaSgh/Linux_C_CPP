f4c961ef0e659c6ebda111396ae454a3db43e5fa
AlphaSgh <upupsgh@163.com>
2023-11-23 15:20:42 +0800

    Linux系统开发FileSystem

--- c_study/IPv4_Stream_project/coding/file_system/mydu.c
+++ c_study/IPv4_Stream_project/coding/file_system/mydu.c
@@ -6,22 +6,59 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <glob.h>
 
+#define PATHSIZE 1024
+
+static int path_noloop(const char *pathname)
+{
+    char *pos;
+    pos = strrchr(pathname, '/');
+    if (pos == NULL)
+    {
+        exit(1);
+    }
+    if (strcmp(pos + 1, ".") == 0 || strcmp(pos + 1, "..") == 0)
+    {
+        return 0;
+    }
+    return 1;
+}
 static int64_t mydu(const char *pathname)
-{ // path 为非目录文件
-    struct stat result_stat;
-    if (lstat(pathname, &result_stat) < 0)
+{
+    // 如果一个变量自始至终没有进入递归中，则可以将该变量声明为static，存储在静态区，不占递归的栈空间
+    // 如果一个变量在递归中使用到，则必须声明为auto类型。
+    // path 为非目录文件
+    static struct stat statres;
+    static char nextpath[PATHSIZE];
+    glob_t globres;
+    if (lstat(pathname, &statres) < 0)
     {
         perror("lstat()");
         exit(1);
     }
-    if (!(S_ISDIR(result_stat.st_mode)))
+    if (!(S_ISDIR(statres.st_mode)))
     {
-        return result_stat.st_blocks / 2;
+        return statres.st_blocks;
     }
+    strncpy(nextpath, pathname, PATHSIZE - 1);
+    strncat(nextpath, "/*", PATHSIZE - 1);
+    glob(nextpath, 0, NULL, &globres);
 
-    // path 为目录文件
-    return 0;
+    strncpy(nextpath, pathname, PATHSIZE - 1);
+    strncat(nextpath, "/.*", PATHSIZE - 1);
+    glob(nextpath, GLOB_APPEND, NULL, &globres);
+
+    int64_t sum = statres.st_blocks;
+    for (size_t i = 0; i < globres.gl_pathc; i++)
+    {
+        if (path_noloop(globres.gl_pathv[i]) != 0)
+        {
+            sum += mydu(globres.gl_pathv[i]);
+        }
+    }
+    globfree(&globres);
+    return sum;
 }
 int main(int argc, char const *argv[])
 {
@@ -31,6 +68,6 @@ int main(int argc, char const *argv[])
         exit(1);
     }
     int64_t res = mydu(argv[1]);
-    printf("%ld\t%s\n", res, argv[1]);
+    printf("%ld\t%s\n", res / 2, argv[1]);
     return 0;
 }
